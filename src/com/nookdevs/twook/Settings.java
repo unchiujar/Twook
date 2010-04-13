081ecbfc4e78f5509d2b7241630d7b1b14753b86
Vasile Jureschi <vasile.jureschi@gmail.com>
2010-04-13 21:25:13 +0300

    add Settings class

---
+++ src/com/nookdevs/twook/Settings.java
@@ -0,0 +1,83 @@
+/***********************************************
+This file is part of the Twook project (**linky**).
+
+    Twook is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Twook is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Twook.  If not, see <http://www.gnu.org/licenses/>.
+
+**********************************************/
+
+package com.nookdevs.twook;
+
+/**
+ * 
+ * Singleton class holding the password and username. 
+ *  
+ *  
+ * @author Vasile Jureschi <vasile.jureschi@gmail.com>
+ * @version 0.0.2
+ * @since 0.0.2
+ * 
+ * @see SettingsActivity
+ * 
+ */
+
+public class Settings {
+	String username;
+	String password;
+	int refresh;
+	private static Settings settings;
+
+	private Settings() {
+		username = "";
+		password = "";
+		refresh = 5;
+	};
+
+	public static synchronized Settings getSettings() {
+		if (settings == null) {
+			settings = new Settings();
+		}
+		return settings;
+	}
+
+	
+	public Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
+	
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getRefresh() {
+		return refresh;
+	}
+
+	public void setRefresh(int refresh) {
+		this.refresh = refresh;
+	}
+
+}
